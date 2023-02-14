5aab30bcbdc4da3728cc2953ee75b1a125d64a6f
lsy3709 <lsy3709@naver.com>
2023-02-15 07:55:41 +0900

    user 분리, 라우팅 설정

---
+++ src/routes/userRoute.js
@@ -0,0 +1,108 @@
+//server.js 에서 파일 분리, user 관련 파일 분리
+//(3)Route 추가 부분
+const { Router } = require('express');
+const userRouter = Router();
+const { User } = require('../models/User')
+
+// 예제1 postman 으로 예제 실습.
+userRouter.get('/', async (req, res) => {
+  try {
+    // 전체 조회.
+    const users = await User.find({});
+    // 하나 조회
+    // const users = await User.findOne({});
+    return res.send({ users: users })
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: err.message })
+  }
+})
+// postman 연동 확인  예제2
+userRouter.post('/', async (req, res) => {
+  try {
+    let { username, name } = req.body;
+    // 아래 코드와 동일 디스트럭쳐
+    // let username = req.body.username;
+    // let name = req.body.name;
+    if (!username) return res.status(400).send({ err: " username is required" });
+    if (!name || !name.first || !name.last) return res.status(400).send({
+      err: "Both first and last names are required"
+    });
+    const user = new User(req.body);
+    await user.save();
+    return res.send({ user })
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+// 예제3 특정 아이디로 조회하는 api 
+// 특정 아이디 변수로 매개변수를 받을 때 -> :userId
+userRouter.get('/:userId', async (req, res) => {
+  // console.log(req.params)
+  try {
+    const { userId } = req.params;
+    // 몽구스 기능에서 해당 아이디 형식에 맞는 유효성 체크 부분 추가.
+    // 포스트 맨 검색시 아이디 부분에, 형식에 맞는 아이디로 조회 하기. 
+    // 예제 localhost:3000/user/63eac2794330a3d7a2704504
+    if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
+    const user = await User.findOne({ _id: userId });
+    return res.send({ user });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+//예제4 특정 유저 삭제하기. 
+userRouter.delete('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
+    const user = await User.findOneAndDelete({ _id: userId });
+    //const user = await User.DeleteOne({_id: userId});  , 삭제 만 하거나, 조회 하고 삭제등 
+    return res.send({ user })
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: err.message })
+  }
+})
+
+//예제5 업데이트 put 
+// 업데이트시 바로 적용이 안되어서 -> {new: true} 추가.
+userRouter.put('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
+
+    // age, name 2개 업데이트 
+    const { age, name } = req.body;
+    if (!age && !name) return res.status(400).send({ err: "age or name is required" });
+
+    // if(!age) return res.status(400).send({err: "age is required"});
+    if (age && typeof age !== 'number') return res.status(400).send({ err: "age must be a number" });
+    if (name && typeof name.first !== 'string' && typeof name.last !== 'string') return res.status(400).send({ err: "first and last name are string" });
+
+    // 아래 코드가 없어도 -> 업데이트 시, name, age 중 하나만 있어도 업데이트 null 업이도 잘됨.
+    //  let updateBody ={};
+    //  if(age) updateBody.age = age;
+    //  if(name) updateBody.name = name;
+    //  const user = await User.findByIdAndUpdate(userId,updateBody,{new: true});
+
+    //대체 { $set: {age}} -> { age}
+    const user = await User.findByIdAndUpdate(userId, { age, name }, { new: true });
+    // const user = await User.findByIdAndUpdate(userId, { $set: {age}},{new: true});
+    return res.send({ user })
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: err.message })
+  }
+})
+// 컴파스에서 필터에서 검색시 아이디가 필요 {_id:ObjectId('63eac7c361ccf46fbdda117d')}
+
+//외부에서 가져쓰기 위해 설정.
+ module.exports = {
+  userRouter
+ }
+
--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const  mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // 몽구스로 래핑해서 마치 컬렉션 처럼 사용하고, 기능이 조금 더 들어가 있음.
 // 몽구스 기능. 
-const { User } = require('./models/User')
+// 라우터 기능 추가로 주석.
+//const { User } = require('./models/User')
+//(3)Route 추가 부분, user 분리된 파일 라우팅 연결 하기.
+const { userRouter } = require('./routes/userRoute')
+
 
 //URI 부분 
 // atlas 또는 도커 부분으로 해도 가능.
@@ -11,122 +15,29 @@ const { User } = require('./models/User')
 //<password> 지우고 패스워드 입력.
 const MONGO_URI = 'mongodb://localhost:27017/BlogService2';
 
+
+
 const server = async () => {
   try {
     // unique 추가시, 인덱스 추가. -> useCreateIndex: true , useFindAndModify:false, 6버전 부터는 안써도 됨. 
+    await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    await mongoose.connect(MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology : true  });
-
-//디버그 하기위한 명령어 쿼리를 콘솔 상에서 확인 가능. 
-mongoose.set('debug', true)
-
+    //디버그 하기위한 명령어 쿼리를 콘솔 상에서 확인 가능. 
+    mongoose.set('debug', true)
     console.log('MongoDB connected')
-  
     app.use(express.json())
-  
-     // 예제1 postman 으로 예제 실습.
-    app.get('/user', async (req, res) => {
-      try {
-        // 전체 조회.
-        const users = await User.find({});
-        // 하나 조회
-        // const users = await User.findOne({});
-        return res.send({ users: users })
-      } catch(err){
-        console.log(err);
-        return res.status(500).send({err : err.message})
-      }
-          })
-    // postman 연동 확인  예제2
-    app.post('/user', async (req, res) => {
-      try {
-        let { username, name} = req.body;
-        // 아래 코드와 동일 디스트럭쳐
-        // let username = req.body.username;
-        // let name = req.body.name;
-        if(!username) return res.status(400).send({err: " username is required"});
-        if(!name || !name.first || !name.last) return res.status(400).send({
-          err: "Both first and last names are required"
-        });
-          const user = new User(req.body);
-          await user.save();
-          return res.send({ user })
-      } catch(err) {
-          console.log(err);
-          return res.status(500).send({err : err.message})
-      }
-    })
-  
-    // 예제3 특정 아이디로 조회하는 api 
-    // 특정 아이디 변수로 매개변수를 받을 때 -> :userId
-    app.get('/user/:userId', async(req,res)=> {
-      // console.log(req.params)
-      try {
-        const { userId } = req.params;
-        // 몽구스 기능에서 해당 아이디 형식에 맞는 유효성 체크 부분 추가.
-        // 포스트 맨 검색시 아이디 부분에, 형식에 맞는 아이디로 조회 하기. 
-        // 예제 localhost:3000/user/63eac2794330a3d7a2704504
-        if(!mongoose.isValidObjectId(userId)) return res.status(400).send({err: "invalid userId"})
-          const user = await User.findOne({_id: userId});
-          return res.send({user});
-      }catch(err){
-        console.log(err);
-        return res.status(500).send({err : err.message})
-      }
-    })
 
-    //예제4 특정 유저 삭제하기. 
-    app.delete('/user/:userId', async (req,res)=> {
-      try {
-        const { userId } = req.params;
-        if(!mongoose.isValidObjectId(userId)) return res.status(400).send({err: "invalid userId"})
-        const user = await User.findOneAndDelete({_id: userId});
-        //const user = await User.DeleteOne({_id: userId});  , 삭제 만 하거나, 조회 하고 삭제등 
-        return res.send({user})
-      }catch(err){
-        console.log(err);
-        return res.status(500).send({err : err.message})
-      }
-    })
+    //미들웨어 추가 , (3)Route 추가 부분
+    app.use('/user', userRouter)
 
-    //예제5 업데이트 put 
-    // 업데이트시 바로 적용이 안되어서 -> {new: true} 추가.
-    app.put('/user/:userId', async(req,res) => {
-      try{
-        const {userId} = req.params;
-        if(!mongoose.isValidObjectId(userId)) return res.status(400).send({err: "invalid userId"})
 
-        // age, name 2개 업데이트 
-        const { age, name } = req.body;
-        if(!age && !name) return res.status(400).send({err: "age or name is required"});
 
-        // if(!age) return res.status(400).send({err: "age is required"});
-        if(age && typeof age !== 'number') return res.status(400).send({err: "age must be a number"});
-        if(name && typeof name.first !== 'string' && typeof name.last !== 'string') return res.status(400).send({err: "first and last name are string"});
-
-        // 아래 코드가 없어도 -> 업데이트 시, name, age 중 하나만 있어도 업데이트 null 업이도 잘됨.
-        //  let updateBody ={};
-        //  if(age) updateBody.age = age;
-        //  if(name) updateBody.name = name;
-        //  const user = await User.findByIdAndUpdate(userId,updateBody,{new: true});
-
-        //대체 { $set: {age}} -> { age}
-        const user = await User.findByIdAndUpdate(userId, { age, name},{new: true});
-        // const user = await User.findByIdAndUpdate(userId, { $set: {age}},{new: true});
-        return res.send({user})
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send({err : err.message})
-      }
-    })
-    // 컴파스에서 필터에서 검색시 아이디가 필요 {_id:ObjectId('63eac7c361ccf46fbdda117d')}
-     
     app.listen(3000, function () {
       console.log('server listening on port 3000');
     })
-  } catch(err) {
+  } catch (err) {
     console.log(err)
   }
-  
+
 }
 server();
