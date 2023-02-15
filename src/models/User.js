47a64a09fb9ac7eedc34fe8ec9df87522dfb3056
lsy3709 <lsy3709@naver.com>
2023-02-16 07:23:20 +0900

    blogRoute 확인중

--- src/models/User.js
+++ src/models/User.js
@@ -10,23 +10,25 @@
 // const mongoose = require('mongoose');
 
 // 디스쳑링 적용후
-const { Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 // 스키마 (데이터베이스 와 비슷)
 const UserSchema = new Schema({
   // username unique 추가 해보기.
-  username : {type : String, requireed: true, unique: true},
+  username: { type: String, requireed: true, unique: true },
   name: {
-    first: {type : String, required: true},
-    last: {type: String, required:true}
+    first: { type: String, required: true },
+    last: { type: String, required: true }
   },
   age: Number,
   email: String
-}, {timestamps: true})
+}, { timestamps: true })
 
 // User 로 해당 모델 user <-> UserSchema 연결
 const User = model('user', UserSchema);
 
 // 외부에서 가져다 쓰기.
-module.exports = { User }
+module.exports = {
+  User
+};
 
