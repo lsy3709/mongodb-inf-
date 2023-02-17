0e8869317cec04340835a3feb95001a78edb8513
lsy3709 <lsy3709@naver.com>
2023-02-17 17:55:54 +0900

    저장1

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -5,6 +5,10 @@ const { Router } = require("express");
 //  Router({ mergeParams: true }); 해결 .
 const commentRouter = Router({ mergeParams: true });
 const { Comment } = require("../models/Comment");
+const { Blog } = require("../models/Blog");
+const { User } = require("../models/User");
+const { isValidObjectId } = require("mongoose");
+
 
 // 조회 localhost:3000/blog/123/comment/456
 /*
@@ -15,7 +19,30 @@ const { Comment } = require("../models/Comment");
 */
 
 commentRouter.post('/:commentId', async (req, res) => {
-  return res.send(req.params);
+  try {
+    const { blogId } = req.params;
+    const { content, userId } = req.body;
+    if (!isValidObjectId(blogId))
+      return res.status(400).send({ err: "blogId is invalid" });
+    if (!isValidObjectId(userId))
+      return res.status(400).send({ err: "userId is invalid" });
+    if (typeof content !== "string")
+      return res.status(400).send({ err: "content is invalid" });
+
+    const blog = await Blog.findByIdAndUpdate(blogId);
+    const user = await User.findByIdAndUpdate(userId);
+
+    if (!blog || !user)
+      return res.status(400).send({ err: "blog or user does not exist" });
+
+    if (!blog.islive) res.status(400).send({ err: "blog is not available" })
+
+    const comment = new Comment({ content, user, blog });
+    return res.send({ comment });
+  } catch (err) {
+    return res.status(400).send({ err: err.message });
+  }
+
 });
 
 commentRouter.get('/')
