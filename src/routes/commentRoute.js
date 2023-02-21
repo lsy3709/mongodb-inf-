d8a6188126c978d6fcbd06b6c613580f42c0639e
lsy3709 <lsy3709@naver.com>
2023-02-21 17:39:58 +0900

    Comment 불러오기

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -29,15 +29,21 @@ commentRouter.post('/', async (req, res) => {
     if (typeof content !== "string")
       return res.status(400).send({ err: "content is required" });
 
-    const blog = await Blog.findByIdAndUpdate(blogId);
-    const user = await User.findByIdAndUpdate(userId);
+    //성능 개선  병렬로 한번에 불러오기.
+    const [blog, user] = await Promise.all([
+      Blog.findByIdAndUpdate(blogId),
+      User.findByIdAndUpdate(userId)
+    ])
+    // const blog = await Blog.findByIdAndUpdate(blogId);
+    // const user = await User.findByIdAndUpdate(userId);
 
     if (!blog || !user)
       return res.status(400).send({ err: "blog or user does not exist" });
 
-    if (!blog.islive) res.status(400).send({ err: "blog is not available" })
+    if (!blog.islive) return res.status(400).send({ err: "blog is not available" })
 
     const comment = new Comment({ content, user, blog });
+    await comment.save();
     return res.send({ comment });
   } catch (err) {
     return res.status(400).send({ err: err.message });
@@ -45,7 +51,15 @@ commentRouter.post('/', async (req, res) => {
 
 });
 
-commentRouter.get('/')
+// comment 불러오기
+commentRouter.get('/', async (req, res) => {
+  const { blogId } = req.params;
+  if (!isValidObjectId(blogId))
+    return res.status(400).send({ err: "blogId is invalid" });
+
+  const comments = await Comment.find({ blog: blogId });
+  return res.send({ comments });
+});
 
 module.exports = {
   commentRouter
