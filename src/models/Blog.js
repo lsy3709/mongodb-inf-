2addb23a393f60e20878774ac4a1568c8d737907
lsy3709 <lsy3709@naver.com>
2023-02-27 12:51:05 +0900

    ch7_postapi 수정 작업중 1

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -8,7 +8,14 @@ const BlogSchema = new Schema(
     title: { type: String, required: true },
     content: { type: String, required: true },
     islive: { type: Boolean, required: true, default: false },
-    user: { type: Types.ObjectId, required: true, ref: "user" },
+    user: {
+      _id: { type: Types.ObjectId, required: true, ref: "user" },
+      username: { type: String, required: true },
+      name: {
+        first: { type: String, required: true },
+        last: { type: String, required: true },
+      },
+    },
   },
   { timestamps: true }
 );
